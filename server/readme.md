ee14a42ace5bdb14a3284d91506ceddc626ee980
Lasse Wallentin <lasse.wallentin@schneider-electric.com>
2020-06-05 15:38:13 +0200

    Added a basic TSED server

---
+++ server/readme.md
@@ -0,0 +1,19 @@
+# Trooper Server - Getting Things Done
+
+
+See [Ts.ED](https://tsed.io) project for more information.
+
+
+## Install
+
+> **Important!** Ts.ED requires Node >= 8, Express >= 4 and TypeScript >= 3.
+
+```batch
+npm install
+```
+
+## Run
+
+```
+npm start
+```
---
+++ server/src/Server.ts
@@ -0,0 +1,68 @@
+import {Configuration, GlobalAcceptMimesMiddleware, Inject, PlatformApplication} from "@tsed/common";
+import "@tsed/swagger";
+import "@tsed/platform-express";
+import * as bodyParser from "body-parser";
+import * as compress from "compression";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
+import * as methodOverride from "method-override";
+import {join} from "path";
+
+const rootDir = __dirname;
+
+@Configuration({
+  rootDir,
+  acceptMimes: ["application/json"],
+  httpPort: process.env.PORT || 8083,
+  httpsPort: false, // CHANGE
+  logger: {
+    debug: true,
+    logRequest: true,
+    requestFields: ["reqId", "method", "url", "headers", "query", "params", "duration"]
+  },
+  mount: {
+    "/rest": [
+      `${rootDir}/controllers/**/*.ts` // Automatic Import, /!\ doesn't works with webpack/jest, use  require.context() or manual import instead
+    ]
+  },
+  componentsScan: [
+    "${rootDir}/mvc/**/*.ts",
+    "${rootDir}/services/**/*.ts",
+    "${rootDir}/middlewares/**/*.ts",
+    "${rootDir}/converters/**/*.ts"
+  ],
+  swagger: [
+    {
+      path: "/api-docs"
+    }
+  ],
+  calendar: {
+    token: true
+  },
+  statics: {
+    "/statics": join(__dirname, "..", "statics")
+  }
+})
+export class Server {
+  @Inject()
+  app: PlatformApplication;
+
+  /**
+   * This method let you configure the middleware required by your application to works.
+   * @returns {Server}
+   */
+  $beforeRoutesInit(): void | Promise<any> {
+    this.app
+      .use(cors())
+      .use(GlobalAcceptMimesMiddleware)
+      .use(cookieParser())
+      .use(compress({}))
+      .use(methodOverride())
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({
+        extended: true
+      }));
+
+    return null;
+  }
+}
