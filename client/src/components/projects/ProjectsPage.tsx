0e615e7331b40f90d1e9f577689b8c05a9f17829
Lasse Wallentin <lasse.wallentin@schneider-electric.com>
2020-06-15 20:56:09 +0200

    Reordered the contents

--- client/src/components/projects/ProjectsPage.tsx
+++ client/src/components/projects/ProjectsPage.tsx
@@ -10,60 +10,20 @@ import { RootState } from '../../redux/reducers';
 import { createProject, deleteProject, fetchProjects } from '../../redux/actions/projectActions';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    buttonNew: {
-      margin: theme.spacing(2),
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }));
-  
-const ProjectsPage = (props: Props) => {
-    const [newProjectOpen, setOpenNewProject] = React.useState(false);
-    const classes = useStyles();
+  root: {
+    flexGrow: 1,
+  },
+  buttonNew: {
+    margin: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-    useEffect(()=> {
-      if (props.dirty) {
-        props.fetchProjects();
-      }
-    }, [props.dirty]);
-   
-    const createProject = async () => {
-      setOpenNewProject(true);
-    }
-
-    const handleNewProject = async (project: ProjectModel | undefined) => {
-
-      if (project) {
-        props.createProject(project);
-      }
-
-      setOpenNewProject(false);
-    }
-
-  return (
-    <div className={classes.root}>
-      <Button variant="contained" color="primary" onClick={createProject} className={classes.buttonNew}>
-          New
-      </Button>
-      <NewProjectDialog open={newProjectOpen} onClose={handleNewProject}></NewProjectDialog>
-      <Grid container spacing={3}>        
-        {props.projects.map(project => (
-          <Grid item xs={12} sm={4} key={project.id}>
-            <ProjectCard project={project} onDeleteProject={(id) => props.deleteProject(id)}></ProjectCard>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  );
-}
-
-const mapState = ({projectsState}: RootState) => {
+const mapState = ({ projectsState }: RootState) => {
   return {
     ...projectsState
   }
@@ -77,7 +37,47 @@ const mapDispatch = {
 
 const connector = connect(mapState, mapDispatch)
 type PropsFromRedux = ConnectedProps<typeof connector>
-
 type Props = PropsFromRedux; // Can add Own Props
+
+
+const ProjectsPage = (props: Props) => {
+  const [newProjectOpen, setOpenNewProject] = React.useState(false);
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (props.dirty) {
+      props.fetchProjects();
+    }
+  }, [props.dirty]);
+
+  const createProject = async () => {
+    setOpenNewProject(true);
+  }
+
+  const handleNewProject = async (project: ProjectModel | undefined) => {
+
+    if (project) {
+      props.createProject(project);
+    }
+
+    setOpenNewProject(false);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Button variant="contained" color="primary" onClick={createProject} className={classes.buttonNew}>
+        New
+      </Button>
+      <NewProjectDialog open={newProjectOpen} onClose={handleNewProject}></NewProjectDialog>
+      <Grid container spacing={3}>
+        {props.projects.map(project => (
+          <Grid item xs={12} sm={4} key={project.id}>
+            <ProjectCard project={project} onDeleteProject={(id) => props.deleteProject(id)}></ProjectCard>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+}
 
 export default connector(ProjectsPage);
